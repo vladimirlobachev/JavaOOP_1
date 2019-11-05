5a21f45a1d4ff9a3bab8e97693bca5213bbe6f84
Vladimir Lobachev <vvlobachev96@gmail.com>
2019-11-05 17:19:23 +0100

    HW_1

---
+++ src/main/java/Homework1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Homework1 {
+    
+    private static int[] readFile() {
+        try {
+            Scanner sc = new Scanner(new File("input.txt"));
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            return new int[] { a, b };
+        } catch (FileNotFoundException ex) {
+            return null;
+        }
+    }
+    
+    private static void writeFile(int number) {
+        try {
+            Writer writer = new BufferedWriter(
+                    new OutputStreamWriter(
+                        new FileOutputStream("output.txt"), "ascii"));
+            writer.write(new Integer(number).toString());
+            writer.close();
+          } catch (IOException ex) {
+              
+          }
+    }
+
+    
+    public static void main(String[] args) {
+        int[] vals = readFile();
+        long mult = vals[0] * vals[1];
+        writeFile(mult);
+    }
+}
---
+++ src/main/java/Task1.java
@@ -0,0 +1,12 @@
+public class Task1 {
+        
+    private static int give_me_one() {
+        return 1;
+    }
+    
+    public static void Main(String[] args) {
+        int one = give_me_one();
+        int two = one * 2;
+        System.out.println(two);
+    }
+}
