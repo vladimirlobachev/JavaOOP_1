6bcf3ecf532e0351bfe0f63521692e382015ea5f
Vladimir Lobachev <vvlobachev96@gmail.com>
2019-11-07 02:55:17 +0100

    got it 5

--- src/main/java/space/harbour/hw2/Pascal.java
+++ src/main/java/space/harbour/hw2/Pascal.java
@@ -15,13 +15,13 @@ public final class Pascal {
 
     private static int getCoeff(final int i, final int j) {
         int res = 1;
-        int new_j = j;
+        int temp = j;
 
         if (j > i - j) {
-            new_j = i - j;
+            temp = i - j;
         }
 
-        for (int k = 0; k < new_j; k++) {
+        for (int k = 0; k < temp; k++) {
             res *= (i - k);
             res /= (k + 1);
         }
